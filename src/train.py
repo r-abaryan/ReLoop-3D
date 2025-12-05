f325f9f855803713c919d1b6c5fe92a2ce8515f1
rasoulcarrera <rasoulcarrera@gmail.com>
2025-12-05 08:56:27 +0000

    small adj

--- src/train.py
+++ src/train.py
@@ -70,9 +70,9 @@ def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--data_dir", type=str, default="data")
 	parser.add_argument("--out_dir", type=str, default="outputs")
-	parser.add_argument("--epochs", type=int, default=5)
+	parser.add_argument("--epochs", type=int, default=10)
 	parser.add_argument("--batch_size", type=int, default=32)
-	parser.add_argument("--lr", type=float, default=1e-3)
+	parser.add_argument("--lr", type=float, default=1e-3) #3e-5
 	parser.add_argument("--val_split", type=float, default=0.2)
 	parser.add_argument("--image_size", type=int, default=128)
 	parser.add_argument("--model", type=str, default="cnn", choices=["cnn", "vit_tiny"]) 
